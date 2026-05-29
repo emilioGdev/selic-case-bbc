5deac2ff7d3a6b647e49e8d19998c448a233e84b
Emilio Gabriel <emiliogabriel000@gmail.com>
2026-05-29 00:29:21 -0300

    fix:corrigindo calculo de taxa acumulada

--- gold/aggregation.py
+++ gold/aggregation.py
@@ -11,7 +11,7 @@ import pandas as pd
 def aggregate_selic_data() -> None:
     """
     Agrupa os dados da Silver e gera indicadores consolidados
-    mensais e anuais da taxa SELIC.
+    mensais e anuais da taxa SELIC utilizando capitalização composta.
     """
     input_path = "/opt/airflow/data/silver/selic_clean.parquet"
     base_dir = "/opt/airflow/data/gold"
@@ -30,22 +30,22 @@ def aggregate_selic_data() -> None:
     df["mes"] = df["data"].dt.month
 
     print("Calculando agregados mensais (Média e Variação)...")
-    
     monthly_df = (
         df.groupby(["ano", "mes"])["valor"].mean().reset_index()
     )
     monthly_df = monthly_df.rename(columns={"valor": "media_mensal"})
-
     monthly_df = monthly_df.sort_values(by=["ano", "mes"]).reset_index(drop=True)
 
     monthly_df["variacao_mensal_pct"] = monthly_df["media_mensal"].pct_change() * 100
-    
     monthly_df["variacao_mensal_pct"] = monthly_df["variacao_mensal_pct"].fillna(0.0)
 
-    print("Calculando taxa acumulada anual...")
+    print("Calculando taxa acumulada anual (Juros Compostos)...")
+    df["fator"] = 1 + (df["valor"] / 100)
     
-    annual_df = df.groupby(["ano"])["valor"].sum().reset_index()
-    annual_df = annual_df.rename(columns={"valor": "acumulado_anual"})
+    annual_df = df.groupby(["ano"])["fator"].prod().reset_index()
+    
+    annual_df["taxa_acumulada_anual"] = (annual_df["fator"] - 1) * 100
+    annual_df = annual_df[["ano", "taxa_acumulada_anual"]]
 
     final_gold_df = pd.merge(monthly_df, annual_df, on="ano", how="left")
 
